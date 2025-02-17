7ba8191b71ef2467dace2335670022f72d3ac484
ankitsinghwakefit <ankitsinghwakefit@gmail.com>
2025-02-17 14:10:19 +0530

    added axios for api calls

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <script setup>
-import { computed } from 'vue'
+import { onMounted,computed } from 'vue'
 import { useStore } from 'vuex'
+import axios from 'axios';
 
 const store = useStore()
 const addProduct = () => store.dispatch('addProduct', { prod: { item: 'com' }})
 const addProductInCart = () => store.dispatch('addProductInCart', { prod: { item: 'com' }})
 const products = computed(() => store.getters.getAllProducts)
 const getCart = computed(() => store.getters.getCart)
+onMounted(async () => {
+  try{
+    let data = await axios.get('https://dummyjson.com/products');
+    console.log(data.data);
+  } catch (error) {
+    let message = error.message;
+    console.log(message);
+}
+})
 </script>
 
 <template>
